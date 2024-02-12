ed74082e26e98a1798c2b5fa9b2bc5f7dac0dd25
Amanda Norris <amandakathleennorris@gmail.com>
2024-02-12 15:49:50 -0600

    module rules in webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -49,6 +49,21 @@ module.exports = () => {
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              options: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
+          }
+        }
         
       ],
     },
